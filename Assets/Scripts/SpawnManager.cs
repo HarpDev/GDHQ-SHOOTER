db32df8e11f40c9fdd913ad8db8d8d37d2f747f7
HarpDev <75390121+HarpDev@users.noreply.github.com>
2022-10-20 16:10:42 -0700

    Triple Shot Power up is fully implemented

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,12 +10,15 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyContainer;
     [SerializeField]
     private float _spawnRate = 0.5f;
+    [SerializeField]
+    private GameObject _tripleShotPowerUpPrefab;
 
     private bool _stopSpawning = false;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
     }
 
     // Update is called once per frame
@@ -36,6 +39,18 @@ public class SpawnManager : MonoBehaviour
             yield return new WaitForSeconds(_spawnRate);
         }
 
+    }
+
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+         while (_stopSpawning == false)
+        {
+            Vector3 postToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            Instantiate(_tripleShotPowerUpPrefab, postToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+
+        }
     }
 
     public void OnPlayerDeath()
